6378c4334893dd921c2297ec5234f49b99ef58c2
Rupali Gangarde <rgangarde@falabella.cl>
2021-08-22 12:47:48 +0530

    Rupali | Basic app setup

---
+++ src/test/java/com/hellofresh/com/hellofreshdemo/HellofreshdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellofresh.com.hellofreshdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellofreshdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
